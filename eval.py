4cc6925c7bc75ff8ad53b71370a712978fe71f2e
Edward Nguyen <tungnguyen99.tn@gmail.com>
2022-06-06 11:21:27 +0700

    update for evaluation on multi_batch

--- eval.py
+++ eval.py
@@ -1,7 +1,8 @@
-from scipy.io import wavfile
 from pesq import pesq   
 from pystoi import stoi
 import argparse 
+import numpy as np
+from tqdm import tqdm
 
 import torch
 import torchaudio
@@ -9,10 +10,11 @@ from torchaudio.transforms import Resample
 
 def get_arg():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--down_sample', type=int, help='down sample rate into 16k Hz')
+    parser.add_argument('--down_sample', type=int, help='down sample rate into 16k Hz', default=1)
     parser.add_argument('--clean', type=str, help='path/to/clean/voice')
     parser.add_argument('--denoised', type=str, help='path/to/denoised/voice')
     parser.add_argument('--metric', type=str, help='pesq or stoi', default='pesq')
+    parser.add_argument('--batch_size', type=int, help='pesq or stoi', default=-1)
     return parser.parse_args()
 
 def load_sample(path: str = None,
@@ -25,19 +27,30 @@ def load_sample(path: str = None,
     signal, rate = torchaudio.load(path)
 
     if down_sample:
-        print("Changing sample rate to 16k Hz...")
         downsampler = Resample(orig_freq=rate, new_freq=16000)
         downsampled_signal = downsampler(signal.view(1, -1))
         signal = downsampled_signal
         rate = 16000
-        print("Sample rate changed!")
+        # print("Sample rate changed into 16kHz!")
     else:
         print("Warning: sample rate = ", rate)
 
     signal = torch.flatten(signal).numpy()
     return signal, rate
 
-def eval_pesq(fs, clean, denoised):
+def get_batches(data, batch_size):
+    batches = []
+    for i in range(0,len(data),batch_size):
+        if len(data) >= batch_size:
+            batches.append(data[i:i+batch_size])
+        else:
+            pass
+    return batches
+
+def eval_pesq(fs: int = 16000, 
+              clean = None, 
+              denoised = None,
+              batch_size = None):
     '''
     clean: ideal audio file
     denoised: noisy voice after performing speech enhancement
@@ -45,13 +58,24 @@ def eval_pesq(fs, clean, denoised):
     ref = clean
     deg = denoised
     rate = fs
-    if rate == 16000:
-        return pesq(rate, ref, deg, 'wb')
-    elif rate == 8000:
-        return pesq(rate, ref, deg, 'nb')
+    if batch_size is not None:
+        scores = []
+        refs = get_batches(ref, batch_size)
+        degs = get_batches(deg, batch_size)
+        for i in tqdm(range(len(refs))):
+            try:
+                scores.append(pesq(rate, refs[i], degs[i], 'wb'))
+            except:
+                pass
+        return np.array(scores).mean()
     else:
-        print("|  Please change sample rating into 16k or 8k")
-        return "N/A"
+        if rate == 16000:
+            return pesq(rate, ref, deg, 'wb')
+        # elif rate == 8000:
+        #     return pesq(rate, ref, deg, 'nb')
+        else:
+            print("|  Please change sample rating into 16k or 8k")
+            return "N/A"
 
 def eval_stoi(fs, clean, denoised):
     '''
@@ -63,27 +87,33 @@ def eval_stoi(fs, clean, denoised):
 def run_eval():
     # Get the test sample
     arg = get_arg()
-    if arg.down_sample==1:
+    if arg.batch_size == -1:
+        batch_size = None
+    else:
+        batch_size = arg.batch_size
+    if arg.down_sample == 1:
         down_sample = True
     else:
         down_sample = False
+
     clean, fs = load_sample(path=arg.clean, down_sample=down_sample)
-    denoised, fs = load_sample(arg.denoised, down_sample=arg.down_sample)
+    denoised, fs = load_sample(arg.denoised, down_sample=down_sample)
+
     min_length = min(clean.shape[0], denoised.shape[0])
     clean = clean[:min_length]
     denoised = denoised[:min_length]
 
     # Print the result to console
-    print("-----------------------------------------------------------------")
-    print("|  Clean voice: ", arg.clean)
-    print("|  Denoised voice: ", arg.denoised)
+    print("Clean voice: ", arg.clean)
+    print("Denoised voice: ", arg.denoised)
     if arg.metric == 'pesq':
-        print("|  PESQ score: ", eval_pesq(fs, clean, denoised))
+        print("Calculating PESQ score...")
+        print("Done! PESQ score: ", eval_pesq(fs, clean, denoised, batch_size))
     elif arg.metric == 'stoi':
-        print("|  STOI score: ", eval_stoi(fs, clean, denoised))
+        print("Evaluation with STOI metric is being maintained, please use PESQ instead")
+        # print("|  STOI score: ", eval_stoi(fs, clean, denoised))
     else:
         print("|  The given metric isn't supported!")
-    print("-----------------------------------------------------------------")
 
 if __name__ == '__main__':
     run_eval()
--- run_eval.sh
+++ run_eval.sh
@@ -1,5 +1,6 @@
 python eval.py \
+    --batch_size -1 \
     --down_sample 1 \
     --metric pesq \
-    --clean /home/thutt36/tungn197/TonSpeech/data/new_samples/clear_sample.mp3\
-    --denoised /home/thutt36/tungn197/TonSpeech/data/new_samples/raw/result_test/keyboard/keyboard_mic_denoised.wav
+    --clean path/to/clean/audio/file \
+    --denoised path/to/denoised/audio/file
